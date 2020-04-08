4dba5876cdbe80b77d25b7cc4681d6ea3ccfd4bf
Sami Barchid <sami.barchid@outlook.com>
2020-04-08 13:42:47 +0200

    begin evaluation

--- evaluater/evaluate_segmentation.py
+++ evaluater/evaluate_segmentation.py
@@ -2,5 +2,9 @@
 """
 
 
-def evaluate(model_file):
-    pass
+def evaluate(model, data_generator):
+    """Evaluates the pixel accuracy, mean pixel accuracy, per-class IoUs and general mIoU of a trained network
+    :param model: the trained Keras model
+    :param data_generator: the instance of SegmentationDataGenerator that generates the test set
+    """
+    
