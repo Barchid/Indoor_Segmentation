1a72d885a8fd3a756ac1bd6181294a8887b7cba0
Sami Barchid <sami.barchid@outlook.com>
2020-04-14 16:49:25 +0200

    add test_args option in get_args

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 import argparse
 
 
-def get_args():
+def get_args(test_args=False):
     argparser = argparse.ArgumentParser(description=__doc__)
     argparser.add_argument(
         '-c', '--config',
@@ -9,5 +9,12 @@ def get_args():
         metavar='C',
         default='None',
         help='The Configuration file')
+    if test_args:
+        argparser.add_argument(
+            '-v', '--visualize',
+            dest='visualize',
+            default='None',
+            help="Flag that indicates whether the images from test must be visualized.s"
+        )
     args = argparser.parse_args()
     return args
