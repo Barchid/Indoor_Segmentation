d62bb8a78c3ae486ca91c96f4fd4330771baba45
Sami Barchid <sami.barchid@outlook.com>
2020-04-16 13:24:49 +0200

    installation sh update for nyuv2

--- installation.sh
+++ installation.sh
@@ -8,28 +8,48 @@
 ####################################################################
 ####################################################################
 
-echo "########################################################################\nCreate SUN-RGBD dataset\n########################################################################"
+echo "########################################################################\nCreate datasets\n########################################################################"
 mkdir datasets
 cd datasets
-mkdir sun_rgbd
-cd sun_rgbd
 
-mkdir SUNRGBD-train_images
-wget http://www.doc.ic.ac.uk/~ahanda/SUNRGBD-train_images.tgz
-tar -xzf SUNRGBD-train_images.tgz -C SUNRGBD-train_images
+echo "########################################################################\nCreate SUN-RGBD dataset\n########################################################################"
+# mkdir sun_rgbd
+# cd sun_rgbd
 
-mkdir SUNRGBD-test_images
-wget http://www.doc.ic.ac.uk/~ahanda/SUNRGBD-test_images.tgz
-tar -xzf SUNRGBD-test_images.tgz -C SUNRGBD-test_images
+# mkdir SUNRGBD-train_images
+# wget http://www.doc.ic.ac.uk/~ahanda/SUNRGBD-train_images.tgz
+# tar -xzf SUNRGBD-train_images.tgz -C SUNRGBD-train_images
 
-mkdir sunrgbd_train_test_labels
-wget https://github.com/ankurhanda/sunrgbd-meta-data/raw/master/sunrgbd_train_test_labels.tar.gz
-tar -xzf sunrgbd_train_test_labels.tar.gz -C sunrgbd_train_test_labels
+# mkdir SUNRGBD-test_images
+# wget http://www.doc.ic.ac.uk/~ahanda/SUNRGBD-test_images.tgz
+# tar -xzf SUNRGBD-test_images.tgz -C SUNRGBD-test_images
 
-wget https://www.doc.ic.ac.uk/~ahanda/sunrgb_train_depth.tgz
-tar -xzf sunrgb_train_depth.tgz
+# mkdir sunrgbd_train_test_labels
+# wget https://github.com/ankurhanda/sunrgbd-meta-data/raw/master/sunrgbd_train_test_labels.tar.gz
+# tar -xzf sunrgbd_train_test_labels.tar.gz -C sunrgbd_train_test_labels
 
-wget https://www.doc.ic.ac.uk/~ahanda/sunrgb_test_depth.tgz
-tar -xzf sunrgb_test_depth.tgz
+# wget https://www.doc.ic.ac.uk/~ahanda/sunrgb_train_depth.tgz
+# tar -xzf sunrgb_train_depth.tgz
 
-python sun_rgbd_dataset_parser.py
+# wget https://www.doc.ic.ac.uk/~ahanda/sunrgb_test_depth.tgz
+# tar -xzf sunrgb_test_depth.tgz
+
+# cd ../..
+# python sun_rgbd_dataset_parser.py
+
+echo "\n########################################################################\nCreate NYU-V2 dataset\n########################################################################\n"
+tar -xf nyu_labels_40.tar.xz
+tar -xf nyu_depths.tar.xz
+mkdir datasets/nyu_v2
+
+mv labels_40 datasets/nyu_v2/
+mv train_depth datasets/nyu_v2/
+mv test_depth datasets/nyu_v2/
+
+cd datasets/nyu_v2
+wget http://www.doc.ic.ac.uk/~ahanda/nyu_train_rgb.tgz
+wget http://www.doc.ic.ac.uk/~ahanda/nyu_test_rgb.tgz
+tar -xzf nyu_train_rgb.tgz
+tar -xzf nyu_test_rgb.tgz
+cd ../..
+python nyu_v2_dataset_parser.py
