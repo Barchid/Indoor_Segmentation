ae909b4f3b4e1d27fc6cee1043b9e7373ac9a8b5
Sami Barchid <sami.barchid@outlook.com>
2020-04-20 22:18:32 +0200

    impelmentation of metrics

--- evaluater/quantitative.py
+++ evaluater/quantitative.py
@@ -1,6 +1,6 @@
 """Evaluates the class-wise IoU, mean IoU, pixel accuracy and class-wise mean pixel accuracy for the test set"""
 import numpy as np
-
+from data_generators.segmentation_data_generator import SegmentationDataGenerator
 
 def get_iou(pred, gt, num_class):
     if pred.shape != gt.shape:
@@ -9,9 +9,8 @@ def get_iou(pred, gt, num_class):
     gt = gt.astype(np.float32)
     pred = pred.astype(np.float32)
 
-    max_label = int(num_class) - 1  # labels from 0,1, ... C
-    count = np.zeros((max_label + 1,))
-    for j in range(1, max_label + 1):
+    count = np.zeros((num_class,))
+    for j in range(0, num_class):
         x = np.where(pred == j)
         p_idx_j = set(zip(x[0].tolist(), x[1].tolist()))
         x = np.where(gt == j)
@@ -23,7 +22,7 @@ def get_iou(pred, gt, num_class):
             count[j] = float(len(n_jj)) / float(len(u_jj))
 
     result_class = count
-    Aiou = np.sum(result_class[:]) / float(len(np.unique(gt))-1)
+    Aiou = np.sum(result_class[:]) / float(len(np.unique(gt)))
     count_class = (result_class != 0)
 
     return Aiou, result_class, count_class
@@ -74,10 +73,9 @@ def mean_pixel_accuracy(pred, gt):
 
 def evaluate_accuracy(model, config):
     num_class = config.model.classes
-    num_correct_lbl, num_samples_lbl = 0, 0
     Aiou = 0
-    Am_acc = 0
-    pixel_acc = 0
+    pixel_acc = 0.
+    mean_pixel_acc = 0.
 
     # Data generator creation
     datagen = SegmentationDataGenerator(config, is_training_set=False)
@@ -103,10 +101,13 @@ def evaluate_accuracy(model, config):
         # retrieve IoUs
         iou, class_iou, class_ct = get_iou(pred, gt, num_class)
 
-        # retrieve pixel accuracy for the current image
+        # retrieve pixel accuracy for the current prediction
         pixel_acc += pixel_accuracy(pred, gt)
 
-        # update means
+        # retrieve mean pixel accuracy for the current prediction
+        mean_pixel_acc += mean_pixel_accuracy(pred, gt)
+
+        # update IoUs stats
         Aiou += iou
         class_Aiou += class_iou
         count_class[class_ct] += 1
@@ -114,48 +115,13 @@ def evaluate_accuracy(model, config):
     # mean of all pixel accuracies
     pixel_acc = pixel_acc / len(datagen)
 
-    for i, (img, lbl) in enumerate(loader):
-        img_var = Variable(img.cuda(), volatile=True)
-        _, _, _, _, _, pred_labels = model(img_var)
+    # mean of all mean pixel accuracies
+    mean_pixel_acc = mean_pixel_acc / len(datagen)
 
-        _, preds_lbl = pred_labels.data.cpu().max(1)
-        lbl = np.squeeze(lbl).numpy()
-        preds_lbl = np.squeeze(preds_lbl).numpy()
-        preds_lbl = misc.imresize(preds_lbl, lbl.shape, mode='F')
-
-        img = img.cpu().numpy()
-        img = img[0].transpose(1, 2, 0)
-        iou, class_iou, class_ct = get_iou(preds_lbl, lbl)
-        Aiou += iou
-        class_Aiou += class_iou
-        count_class[class_ct] += 1
-        print("processing: %d/%d" % (i, len(loader)))
-
-        lbl_0 = (lbl == 0)
-        preds_lbl[lbl_0] = 0
-        # save images
-        # plt.imsave(os.path.join(out_prdlbl,"%d.png"%(i+1)),preds_lbl*4,cmap='nipy_spectral',vmin=0,vmax=(num_class-1)*4)
-
-        t_cls = np.unique(lbl)
-        if t_cls[0] == 0:
-            t_cls = t_cls[1:]
-
-        mask_lbl = (lbl != 0)
-        lbl = torch.from_numpy(lbl[mask_lbl]).long()
-        preds_lbl = torch.from_numpy(preds_lbl[mask_lbl]).long()
-        num_correct_lbl += (preds_lbl.long() == lbl.long()).sum()
-        num_samples_lbl += preds_lbl.numel()
-
-        m_acc = 0
-        for cls in t_cls:
-            m_acc += float((preds_lbl[lbl.long() == cls].long()
-                            == cls).sum())/float((lbl.long() == cls).sum())
-        Am_acc += m_acc/len(t_cls)
-
-    acc_lbl = float(num_correct_lbl)/num_samples_lbl
+    # finish equations for IoUs
     Aiou /= num_samples
-    Am_acc /= num_samples
-    class_Aiou[1:] /= count_class[1:]
+    class_Aiou[:] /= count_class[:]
+    # remove div by 0 and replace them by 0
     class_Aiou = np.where(np.isnan(class_Aiou), 0, class_Aiou)
 
-    return Aiou, Am_acc, acc_lbl, class_Aiou
+    return Aiou, class_Aiou, pixel_acc, mean_pixel_acc
