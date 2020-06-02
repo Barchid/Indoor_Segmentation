bc41c0524573f0aa9401c07d8956afaec710b245
Sami Barchid <sami.barchid@outlook.com>
2020-06-02 15:30:15 +0200

    add param for seg grad cam

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 import argparse
 
 
-def get_args(test_args=False):
+def get_args(test_args=False, seg_grad_cam=False):
     argparser = argparse.ArgumentParser(description=__doc__)
     argparser.add_argument(
         '-c', '--config',
@@ -16,6 +16,20 @@ def get_args(test_args=False):
             action='store_true',
             help="Flag that indicates whether the images from test must be visualized.s"
         )
-    
+
+    if seg_grad_cam:
+        argparser.add_argument(
+            '-l', '--layer',
+            dest='layer',
+            default='None',
+            help='Layer to extract seg grad cam')
+
+        argparser.add_argument(
+            '--class_id',
+            dest='class_id',
+            required=True,
+            help='class id to analyse with SEG grad cam'
+        )
+
     args = argparser.parse_args()
     return args
