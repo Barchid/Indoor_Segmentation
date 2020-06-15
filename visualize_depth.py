6c35667bd9bbedaec96a0680b6cacbd729973d9b
Sami Barchid <sami.barchid@outlook.com>
2020-06-15 15:46:44 +0200

    use a gradcam variant for depth estimation

--- visualize_depth.py
+++ visualize_depth.py
@@ -2,6 +2,7 @@ from models.fpn_depth import FpnDepth
 from utils import factory
 from data_generators.depth_data_generator import DepthDataGenerator
 from utils.config import process_config
+from utils.depth_grad_cam import DepthGradCam
 import cv2
 import numpy as np
 from utils.utils import get_args
@@ -9,15 +10,19 @@ import imgaug as ia
 import imgaug.augmenters as iaa
 
 
-def create_visualization(pred, gt, img):
+def create_visualization(pred, gt, img, heatmap, overlay):
     pred = cv2.cvtColor(pred, cv2.COLOR_GRAY2BGR)
     gt = cv2.cvtColor(gt, cv2.COLOR_GRAY2BGR)
-    
+    heatmap = heatmap.astype(np.float32)/255.
+    overlay = overlay.astype(np.float32)/255.
+
     grid_image = ia.draw_grid([
         img,
         pred,
-        gt
-    ], cols=3)
+        gt,
+        heatmap,
+        overlay
+    ], cols=5)
     return grid_image
 
 
@@ -31,29 +36,22 @@ def visualize_results(model, config, datagen):
         X, Y = datagen[i]
         prediction = model.predict(X, batch_size=1, verbose=1)
         prediction = np.clip(prediction[0], 0., 1.)
-        visualization = create_visualization(prediction, Y[0], X[0])
+
+        gradcam = DepthGradCam(model, depth_range=(
+            config.validation.depth_range.start, config.validation.depth_range.end),
+            layer_name=config.validation.layer_name)
+
+        heatmap = gradcam.compute_heatmap(X)
+        heatmap, overlay = gradcam.overlay_heatmap(heatmap, X[0].copy())
+
+        visualization = create_visualization(
+            prediction, Y[0], X[0], heatmap, overlay)
 
         # display
         cv2.imshow('Image - Ground truth - Prediction', visualization)
         if cv2.waitKey() == ord('a'):
             print('STOP')
             exit()
-
-        if config.generator.depth_dir is None:
-            X, Y = datagen[i]
-            prediction = model.predict(
-                X, batch_size=config.trainer.batch_size, verbose=1)
-
-            visualization = create_visualization(X[0], prediction[0], Y[0])
-
-            cv2.imshow('Image - Ground truth - Prediction', visualization)
-            if cv2.waitKey() == ord('a'):
-                print('STOP')
-                exit()
-
-        else:
-            # TODO : depth dir
-            pass
 
 
 def load_weights(model, config):
