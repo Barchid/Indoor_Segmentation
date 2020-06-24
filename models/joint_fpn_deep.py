c8e98a9501932b02c7b2b5a758c52f0bd334cace
Sami Barchid <sami.barchid@outlook.com>
2020-06-24 18:29:22 +0200

    bi fpn update

--- models/joint_fpn_deep.py
+++ models/joint_fpn_deep.py
@@ -7,15 +7,13 @@ from losses.focal_loss import CategoricalFocalLoss
 from losses.lovasz_softmax import MultiClassLovaszSoftmaxLoss
 from keras import backend as K
 from models.backbones.df import DF1, DF2
-from layers.bi_fpn import BiFpnDeepLayer
 
 # name of the layers used for the skip connections in the top-down pathway
 skip_connections = {
     'mobilenet_v2': (
         'block_13_expand_relu',  # stride 16
         'block_6_expand_relu',  # stride 8
-        'block_3_expand_relu',   # stride 4
-        'block_1_expand_relu'   # stride 2
+        'block_3_expand_relu'   # stride 4
     ),
     'resnet18': (
 
@@ -23,8 +21,7 @@ skip_connections = {
     'resnet50': (
         'conv4_block6_out',  # stride 16
         'conv3_block4_out',  # stride 8
-        'conv2_block3_out',  # stride 4
-        'conv1_relu'  # stride 2
+        'conv2_block3_out'  # stride 4
     ),
     'resnet101': (
         'conv4_block23_out',  # stride 16
@@ -34,52 +31,86 @@ skip_connections = {
     'DF1': (
         'tf_op_layer_Relu_13',  # stride 16
         'tf_op_layer_Relu_7',  # stride 8
-        'tf_op_layer_Relu_1',  # stride 4
-        'tf_op_layer_Relu'  # stride 2
+        'tf_op_layer_Relu_1'  # stride 4
     ),
     'DF2': (
         'tf_op_layer_Relu_29',  # stride 16
         'tf_op_layer_Relu_7',  # stride 8
-        'tf_op_layer_Relu_1',  # stride 4
-        'tf_op_layer_Relu'  # stride 2
+        'tf_op_layer_Relu_1'  # stride 4
     ),
 }
 
 
-class BiFpnDeep(BaseModel):
+class JointFpnDeep(BaseModel):
     def __init__(self, config, datagen):
         self.datagen = datagen
-        super(BiFpnDeep, self).__init__(config)
+        super(JointFpnDeep, self).__init__(config)
 
     def build_model(self):
         # backbone encoder
         backbone, skips = self.get_backbone()
 
-        # decoder construction
-        prediction = self.create_decoder(backbone, skips)
+        # joint decoder (resolution 1/4)
+        decoder = self.decoder(backbone, skips)
 
+        # segmentation head
+        segmentation_mask = self.segmentation_head(decoder)
+
+        # depth estimation head
+        depth_estimation = self.depth_head(decoder)
+
+        # create model
         network = keras.Model(
-            inputs=backbone.input, outputs=prediction, name="BI_FPN_NET")
+            inputs=backbone.input,
+            outputs=[
+                segmentation_mask,
+                depth_estimation
+            ],
+            name="JOINT_FPN"
+        )
 
         network.summary()
 
         # get the optimizer
         optimizer = self.build_optimizer()
 
-        metrics = self.build_metrics_NYU()
-
+        # define segmentation loss
         if self.config.model.loss == "focal_loss":
-            loss = CategoricalFocalLoss(
+            seg_loss = CategoricalFocalLoss(
                 gamma=self.config.model.gamma,
                 alpha=self.config.model.alpha
             )
         elif self.config.model.loss == "lovasz":
-            loss = MultiClassLovaszSoftmaxLoss()
+            seg_loss = MultiClassLovaszSoftmaxLoss()
         else:
-            loss = 'categorical_crossentropy'
+            seg_loss = 'categorical_crossentropy'
 
-        network.compile(loss=loss,
-                        optimizer=optimizer, metrics=metrics)
+        # define depth loss
+        depth_loss = tf.keras.losses.MeanSquaredError()
+
+        # composed loss
+        losses = {
+            "seg_out": seg_loss,
+            "dep_out": depth_loss
+        }
+
+        loss_weights = {
+            "seg_out": self.config.model.loss_weights.seg_loss,
+            "dep_out": self.config.model.loss_weights.depth_loss
+        }
+
+        # define metrics
+        metrics = {
+            "seg_out": self._generate_metrics(),
+            "dep_out": [tf.keras.metrics.RootMeanSquaredError()]
+        }
+
+        network.compile(
+            loss=losses,
+            loss_weights=loss_weights,
+            optimizer=optimizer,
+            metrics=metrics
+        )
 
         return network
 
@@ -165,32 +196,40 @@ class BiFpnDeep(BaseModel):
 
         return backbone_fn
 
-    def create_decoder(self, backbone, skips):
+    def decoder(self, backbone, skips):
         stage5 = backbone.output  # resolution 1/32
         stage4 = skips[0]  # resolution 1/16
         stage3 = skips[1]  # resolution 1/8
         stage2 = skips[2]  # resolution 1/4
-        stage1 = skips[3]  # resolution 1/2
 
         # Pyramid pooling module for stage 5 tensor
-        stage5 = ppm_block(stage5, (1, 2, 3, 6), 128, 512)
+        stage5 = ppm_block(stage5, (1, 2, 3, 6), 128, 1024)
 
-        # 3x BI_FPN blocks
-        P1, P2, P3, P4, P5 = BiFpnDeepLayer(
-            stage1, stage2, stage3, stage4, stage5, filters=128, conv_input=True, name="BiFpn_0_")
-        P1, P2, P3, P4, P5 = BiFpnDeepLayer(
-            P1, P2, P3, P4, P5, filters=128, conv_input=True, name="BiFpn_1_")
-        P1 = BiFpnDeepLayer(
-            P1, P2, P3, P4, P5, filters=128, conv_input=True, has_bottomup=False, name="BiFpn_2_")
+        # channel controllers
+        skip4 = conv2d(stage4, 512, 1, 1, kernel_size=1,
+                       use_relu=True, name="decoder_skip4")
+        skip3 = conv2d(stage3, 256, 1, 1, kernel_size=1,
+                       use_relu=True, name="decoder_skip3")
+        skip2 = conv2d(stage2, 128, 1, 1, kernel_size=1,
+                       use_relu=True, name="decoder_skip2")
 
-        prediction = self.segmentation_head(P1)
-        return prediction
+        # fusion nodes
+        fusion4 = fusion_node(stage5, skip4, name="decoder_fusion4")
+        fusion3 = fusion_node(fusion4, skip3, name="decoder_fusion3")
+        fusion2 = fusion_node(fusion3, skip2, name="decoder_fusion2")
+        print(fusion2.shape)
+
+        # fusion nodes merging
+        merge = merge_block(fusion4, fusion3, fusion2,
+                            out_channels=64)
+        print(merge.shape)
+        return merge
 
     def segmentation_head(self, features):
         features = conv2d(features, 64, 1, 1, kernel_size=3,
-                          name="segmentation_head_3x3")
+                          use_relu=True, name="segmentation_head_3x3")
         features = conv2d(features, self.config.model.classes,
-                          1, 1, kernel_size=1, name="segmentation_head_1x1")
+                          1, 1, kernel_size=1, use_relu=True, name="segmentation_head_1x1")
 
         upsampled = resize_img(
             features, self.config.model.height, self.config.model.width)
@@ -199,26 +238,39 @@ class BiFpnDeep(BaseModel):
             'softmax', name='seg_out')(upsampled)
         return segmentation_mask
 
+    def depth_head(self, features):
+        features = conv2d(features, 64, 1, 1, kernel_size=3,
+                          use_relu=True, name="depth_head_3x3")
+        features = conv2d(features, 1,
+                          1, 1, kernel_size=1, use_relu=True, name="depth_head_1x1")
+
+        upsampled = resize_img(
+            features, self.config.model.height, self.config.model.width, name="dep_out")
+
+        return upsampled
+
 # Layer functions
 
 
-def merge_block(stage4, stage3, stage2, out_channels):
+def merge_block(fusion4, fusion3, fusion2, out_channels):
     # get fusion2 channels number
-    inter_channels = K.int_shape(stage2)[-1]
+    inter_channels = K.int_shape(fusion2)[-1]
 
     # fusion3 upsampling
-    stage3 = conv2d(stage3, inter_channels, 1, 1, kernel_size=1)
-    stage3 = UpSampling2D(size=(2, 2))(stage3)
+    fusion3 = conv2d(fusion3, inter_channels, 1, 1,
+                     kernel_size=1, use_relu=True)
+    fusion3 = UpSampling2D(size=(2, 2))(fusion3)
 
     # fusion4 upsampling
-    stage4 = conv2d(stage4, inter_channels, 1, 1, kernel_size=1)
-    stage4 = UpSampling2D(size=(4, 4))(stage4)
+    fusion4 = conv2d(fusion4, inter_channels, 1, 1,
+                     kernel_size=1, use_relu=True)
+    fusion4 = UpSampling2D(size=(4, 4))(fusion4)
 
     # addition
     merge = Add()([
-        stage4,
-        stage3,
-        stage2
+        fusion4,
+        fusion3,
+        fusion2
     ])
 
     # last conv layer
@@ -243,29 +295,56 @@ def ppm_block(input, bin_sizes, inter_channels, out_channels):
             pool_size=(H//bin_size, W//bin_size),
             strides=(H//bin_size, W//bin_size)
         )(input)
-        x = conv2d(x, inter_channels, 1, 1, kernel_size=1)
+        x = conv2d(x, inter_channels, 1, 1, kernel_size=1, use_relu=True)
         x = Lambda(lambda x: tf.image.resize(x, (H, W)))(x)
         concat.append(x)
     x = concatenate(concat)
-    x = conv2d(x, out_channels, 1, 1, 3)
+    x = conv2d(x, out_channels, 1, 1, 3, use_relu=True)
     return x
 
 
-def conv2d(input, filters, stride, n, kernel_size=3, name=None):
+def fusion_node(low_res, high_res, name=None):
+    # define names
+    if name is not None:
+        low_conv_name = name + "_low"
+        low_up_name = name + "_low_upsample"
+        fus_name = name + "_concat"
+        fus_conv_name = name + "_final"
+    else:
+        low_conv_name = low_up_name = fus_name = fus_conv_name = None
+
+    # channel depth of output is high_res feature map depth
+    out_channels = K.int_shape(high_res)[-1]
+
+    # upsample the deepest, low-resolution feature map
+    low_res = conv2d(low_res, out_channels, 1, 1, kernel_size=1,
+                     use_relu=True, name=low_conv_name)
+    low_res = UpSampling2D(size=(2, 2), name=low_up_name)(low_res)
+
+    # fusion of two tensors
+    fusion = concatenate([low_res, high_res], name=fus_name)
+    fusion = conv2d(fusion, out_channels, 1, 1, kernel_size=3,
+                    use_relu=True, name=fus_conv_name)
+
+    return fusion
+
+
+def conv2d(input, filters, stride, n, kernel_size=3, use_relu=True, name=None):
     x = input
     for i in range(n):
         # define names for layers
         if name is not None:
             conv_name = name + "_conv_" + str(i)
             bn_name = name + "_bn_" + str(i)
-            swish_name = name + "_swish_" + str(i)
+            relu_name = name + "_relu_" + str(i)
         else:
-            conv_name = bn_name = swish_name = None
+            conv_name = bn_name = relu_name = None
 
         x = Conv2D(filters, (kernel_size, kernel_size), strides=(
             stride, stride), padding="same", name=conv_name)(x)
 
         x = BatchNormalization(name=bn_name)(x)
 
-        x = Activation('swish', name=swish_name)(x)
+        if use_relu:
+            x = Activation('relu', name=relu_name)(x)
     return x
